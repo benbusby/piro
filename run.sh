1fb9ed24f7159a8a9d470f16763615352b412c96
Ben Busby <33362396+benbusby@users.noreply.github.com>
2019-04-18 13:31:24 -0600

    Refactored run script

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 network_ip=(`hostname -I`)
 
-if [[ $# -eq 0 ]]; then
+if [[ $# -eq 0 ]] || [[ $1 == "remote" ]]; then
+    # Optionally run janus (can be skipped if not testing streaming)
     while true; do
         read -p "Run Janus Gateway? (y/n) " yn
         case $yn in
@@ -16,11 +17,17 @@ if [[ $# -eq 0 ]]; then
                 ;;
         esac
     done
-
-    python routes.py 0.0.0.0 #${network_ip[0]}
-elif [[ $1 == "remote" ]]; then
-    sudo -u pi -H bash -c "/opt/janus/bin/janus --debug-level=7 -F /home/nvidia/JetsonStreaming/janus_conf &"
-    sudo -u pi -H bash -c ". /home/nvidia/JetsonStreaming/streaming/venv/bin/activate; python /home/nvidia/JetsonStreaming/streaming/routes.py ${network_ip[0]} &"
+    
+    # Start flask server either on local network or public
+    if [[ $# -eq 0 ]]; then
+        python routes.py ${network_ip[0]}
+    elif [[ $1 == "remote" ]]; then
+        python routes.py # default ip in routes is already 0.0.0.0
+elif [[ $1 == "service" ]]; then
+    # Start up all necessary services if executing as a systemd service
+    sudo -u pi -H bash -c "pigpiod"
+    sudo -u pi -H bash -c "/opt/janus/bin/janus &"
+    sudo -u pi -H bash -c ". /home/pi/raztot/venv/bin/activate; python /home/pi/raztot/routes.py &"
 else
     echo "Invalid argument provided."
     exit 1
