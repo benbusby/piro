fe5d81ebfbf24114233790f040d3716983026393
Ben Busby <benbusby@protonmail.com>
2020-12-16 18:35:07 -0500

    Simplify gpio model, shorten enum names
    GPIO interface has been updated with better enum names ("Pins" instead
    of "SERVO_PINS" and "PWM" instead of "PWM_VALUES").
    
    Some minor rearranging for better logical flow of functions.

--- app/models/gpio.rb
+++ app/models/gpio.rb
@@ -1,6 +1,7 @@
 require "rpi_gpio"
 
-module SERVO_PINS
+# Note: Change these if you aren't using pins 17 and 22
+module Pins
   LEFT = 17
   RIGHT = 22
 end
@@ -10,32 +11,38 @@ end
 # and backward.
 #
 # CW -> clockwise, CCW -> counter-clockwise
-module PWM_VALUES
+module PWM
   CW = 2000
   CCW = 1000
   OFF = 0
 end
 
+# Interface between the server and GPIO PWM servos
 class InterfaceGPIO
   def initialize
-    RPi::GPIO.setup [SERVO_PINS::LEFT, SERVO_PINS::RIGHT], :as => :output
-    @pwm_l = RPi::GPIO::PWM.new(SERVO_PINS::LEFT, 0)
-    @pwm_r = RPi::GPIO::PWM.new(SERVO_PINS::RIGHT, 0)
-  end
-
-  def command(data)
-    if data == nil
-      move(PWM_VALUES::OFF, PWM_VALUES::OFF)
-    elsif data["left"] || data["right"]
-      move(data["left"] * PWM_VALUES::CCW,
-           data["right"] * PWM_VALUES::CW)
-    else
-      move(data["down"] ? PWM_VALUES::CCW : PWM_VALUES::CW,
-           data["down"] ? PWM_VALUES::CW : PWM_VALUES::CCW)
-    end
+    puts "Initializing GPIO interface..."
+    RPi::GPIO.setup [Pins::LEFT, Pins::RIGHT], :as => :output
+    @pwm_l = RPi::GPIO::PWM.new(Pins::LEFT, 0)
+    @pwm_r = RPi::GPIO::PWM.new(Pins::RIGHT, 0)
+    puts "GPIO interface ready to use!"
   end
 
   def move(left, right)
     @pwm_l.frequency, @pwm_r.frequency = left, right
+    puts "PWM_L::%d | PWM_R::%d" % [left, right]
+  end
+
+  def command(data)
+    if data == nil
+      move(PWM::OFF, PWM::OFF)
+    elsif data[:left] || data[:right]
+
+      move(data[:left] * PWM::CCW,
+           data[:right] * PWM::CW)
+    elsif data[:up] || data[:down]
+      move(data[:down] ? PWM::CCW : PWM::CW,
+           data[:down] ? PWM::CW : PWM::CCW)
+    end
   end
 end
+
