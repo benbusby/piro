7c4f4a650f4bfec4496b45c2f6fbddf543ee2f6d
Ben Busby <benbusby@protonmail.com>
2021-06-06 15:23:22 -0400

    Finish basic servo controls
    Wraps up preliminary implementation of user provided servo commands. The
    intent is that the keyup and keydown presses will send a json string to
    the server, which can be parsed into a simple struct and used to command
    the motors.
    
    Uses two functions, servo_control and send_command, which determine the
    servo state and send the correct value to the gpio daemon respectively.

--- src/servos.rs
+++ src/servos.rs
@@ -2,22 +2,65 @@ use {
     std::{
         env,
         process::Command,
+    },
+    serde::{
+        Deserialize,
+        Serialize
     }
 };
 
-// PWM values
-const CW: &str = "1000";
-const CCW: &str = "2000";
-const STOP: &str = "0";
+// A struct to mirror user keydown/keyup events
+#[derive(Serialize, Deserialize)]
+struct ServoCommand {
+    left: u16,
+    down: u16,
+    up: u16,
+    right: u16
+}
 
-fn servo_control(left: bool, right: bool) {
-    // Servo pin #s
-    // TODO: Unfinished, needs refactoring
+// PWM values
+const CW: u16 = 1000;
+const CCW: u16 = 2000;
+const STOP: u16 = 0;
+
+/// Parses json servo commands from the websocket connection, and interprets them
+/// for the pi gpio daemon.
+///
+/// # Arguments
+/// * `data` - the raw (json) string sent by the user. If a key is pressed, it will
+///            be sent as a 1, otherwise it will be a 0
+fn servo_control(data: &str) {
+    let command: ServoCommand = serde_json::from_str(data).unwrap();
+
+    if command.left != 0 || command.right != 0 {
+        send_command(command.left * CW, command.right * CCW);
+    } else if command.up != 0 || command.down != 0 {
+        send_command(
+            if command.up != 0 { CCW } else { CW },
+            if command.up != 0 { CW } else { CCW }
+        )
+    } else {
+        send_command(STOP, STOP)
+    }
+}
+
+/// Sends the PWM value for the servo to "pigs" (pi gpio daemon must be running).
+///
+/// Note: This is assuming a mirrored motor setup for either side of the vehicle, meaning one side
+/// should turn clockwise and the other should turn counterclockwise to move forward, and the
+/// opposite for reversing. Turning is accomplished by moving the opposite wheel (turning left
+/// moves the right wheel and uses the left wheel as a pivot, and vice versa).
+///
+/// # Arguments
+/// * `l_val` - the PWM value for the "left" servo
+/// * `r_val` - the PWM value for the "right" servo
+fn send_command(l_val: u16, r_val: u16) {
     let servo_l: &str = &*env::var("servo_l").unwrap_or("17".to_string());
     let servo_r: &str = &*env::var("servo_r").unwrap_or("27".to_string());
 
     Command::new("pigs")
-        .args(&["SERVO", servo_l, if left { CW } else { STOP }])
-        .args(&["SERVO", servo_r, if right { CCW } else { STOP }])
-        .output();
+        .args(&["SERVO", servo_l, &l_val.to_string()])
+        .args(&["SERVO", servo_r, &r_val.to_string()])
+        .output()
+        .expect("Failed to execute servo command");
 }
